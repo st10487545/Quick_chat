a55802af78a868518d1781b47582b70e4c89bfb1
st10487545 <ST10487545@VCCONNECT.EDU.ZA>
2026-04-08 18:45:33 +0200

    COMMIT 1 : QUICK CHAT
    Created main class quick chat

---
+++ src/main/java/com/mycompany/quick_chat/Quick_chat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quick_chat;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Quick_chat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
