77762fb3887d036034784e7e109b1dae357e67e8
st10487545 <ST10487545@VCCONNECT.EDU.ZA>
2026-04-08 18:49:37 +0200

    COMMIT 02 : Created test and login class
    Created other classes

--- src/main/java/com/mycompany/quick_chat/Test.java
+++ src/main/java/com/mycompany/quick_chat/Test.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.quick_chat;
 
 /**
  *
  * @author lab_services_student
  */
-public class Quick_chat {
-
-    public static void main(String[] args) {
-        
-    }
+public class Test {
+    
 }
