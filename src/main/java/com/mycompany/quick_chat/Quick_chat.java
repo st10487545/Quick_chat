c879de48c896641d4cf02fd05dae8f423bd4dfcf
st10487545 <ST10487545@VCCONNECT.EDU.ZA>
2026-04-08 19:22:51 +0200

    COMMIT 03 : UPDATED CODE
    Updated code for main quickchat class

--- src/main/java/com/mycompany/quick_chat/Quick_chat.java
+++ src/main/java/com/mycompany/quick_chat/Quick_chat.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.quick_chat;
 
+import java.util.Scanner;
+
 /**
  *
  * @author lab_services_student
@@ -11,6 +13,32 @@ package com.mycompany.quick_chat;
 public class Quick_chat {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        
+        System.out.println("QuickChat Registration");
+        
+        System.out.println("Enter First Name");
+        String firstName = scanner.nextLine();
+        
+        System.out.println("Enter Last Name ");
+        String lastName = scanner.nextLine();
+        
+        System.out.println("Enter Username");
+        String username =  scanner.nextLine();
+        
+        System.out.println("Enter Cell Phone Number");
+        String cellNumber = scanner.nextLine();
+        
+        Login login = new Login(firstName,lastName,username,cellNumber);
+        
+        System.out.println("\n + login.registerUser()");
+        
+        if (login.checkUserName() && login.checkPasswordComplexity()&& login.checkCellPhoneNumber()){
+            System.out.println("\n Quickchat Login");
+        }
+        
+        
+        
         
     }
 }
