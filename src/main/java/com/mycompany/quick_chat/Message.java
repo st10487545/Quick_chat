a91efe1a38a27ec5c90010d6563b9d51a235d244
st10487545 <ST10487545@VCCONNECT.EDU.ZA>
2026-04-13 19:24:33 +0200

    COMMIT 12 added and imported array list

--- src/main/java/com/mycompany/quick_chat/Message.java
+++ src/main/java/com/mycompany/quick_chat/Message.java
@@ -4,10 +4,31 @@
  */
 package com.mycompany.quick_chat;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author lab_services_student
  */
 public class Message {
-    
+    private String messageID;
+    private int messageNumber;
+    private String recipient;
+    private String message;
+    private String messageHash;
+
+    // These lists keep track of all messages during the session
+    private static ArrayList<String> sentMessages = new ArrayList<>();
+    private static ArrayList<String> messageHashes = new ArrayList<>();
+    private static ArrayList<String> messageIDs = new ArrayList<>();
+    private static int totalMessagesSent = 0;
+
+    // Constructor sets up the message and auto-generates the ID and hash
+    public Message(int messageNumber, String recipient, String message) {
+        this.messageNumber = messageNumber;
+        this.recipient = recipient;
+        this.message = message;
+        this.messageID = generateMessageID();
+        this.messageHash = createMessageHash();
+    }
 }
